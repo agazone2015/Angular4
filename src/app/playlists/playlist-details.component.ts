891190bec3c9c589abb6081907ac865bcb4c08e1
Agnes Carroll <digicraft7@hotmail.com>
2017-07-04 12:17:08 +0200

    wiazania

--- src/app/playlists/playlist-details.component.ts
+++ src/app/playlists/playlist-details.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Playlist} from './interfaces'
 @Component({
   selector: 'sg-playlist-details',
   template: `
-    <p>
-      playlist-details Works!
-    </p>
+<div>
+  <p>Name {{playlist.name}}</p>
+  <p> {{playlist.favourite? "Favourite" : "Not favourite"}}</p>
+  <p [style.color]="playlist.color">Color</p>
+</div>
+
+  <div>
+    <div class="form-group">
+      <label>Nazwa: </label>
+        <input type="text" [value]="playlist.name" class="form-control" (keyup)=" playlist.name = $event.target.value "> <!--przekazuje obiect-->
+    </div>
+
+    <div class="form-group">
+      <label>Favourite: </label>
+        <input type="checkbox" [checked]="playlist.favourite" (change)=" playlist.favourite = $event.target.checked "> <!--przekazuje stringa-->
+    </div>
+
+    <div class="form-group">
+      <label>Color: </label>
+        <input type="color" [value]="playlist.color" (change)=" playlist.color = $event.target.value ">>
+
+        <button class="btn btn-default">Cancel</button>
+        <button class="btn btn-success">Save</button>
+    </div>
+  </div>
   `,
   styles: []
 })
 export class PlaylistDetailsComponent implements OnInit {
+
+  playlist:Playlist = {
+    id: null,
+    name: 'Angular Hits',
+    color: '#ff0000',
+    favourite: true
+  }
 
   constructor() { }
 
--- src/app/playlists/playlists-list.component.ts
+++ src/app/playlists/playlists-list.component.ts
@@ -4,50 +4,23 @@ import { Component, OnInit } from '@angular/core';
   selector: 'sg-playlists-list',
   template: `
     <div class="list-group">
-      <div class="list-group-item">{{message}}</div>
-      <div class="list-group-item">{{data.name}}</div>
-      <div class="list-group-item">{{counter % 2 == 0?'even' : 'odd'}}</div>
-      <div class="list-group-item" [style.borderColor] = "counter % 2 == 0? 'blue' : data.color">Test</div>
-      <div class="list-group-item">Test</div>
-      <div class="list-group-item">Test</div>
-      <div class="list-group-item">Test</div>
-      <div class="list-group-item">Test</div>
+      <div class="list-group-item" [class.active]="selected" (click)=" selected = !selected ">Test</div>
     </div>
   `,
-  styles: [`
-    :host {
-      border: 1px solid black;
-      display: block;
-    }
+  styles: [
 
-    :host >>> p {
-      color: blue;
-    }
-
-    p {
-      color: red;
-    }
-  `]
+  ]
 })
 export class PlaylistsListComponent implements OnInit {
 
-  message = 'Wiadomosc'
-
-  data =  {
-    name: 'Alice',
-    color: "red"
-  }
-
-  counter = 0
+  selected = true;
 
   constructor() { 
     
   }
 
   ngOnInit() {
-    setInterval(() => {
-      this.counter++;
-    }, 2500)
+    
   }
 
 }
